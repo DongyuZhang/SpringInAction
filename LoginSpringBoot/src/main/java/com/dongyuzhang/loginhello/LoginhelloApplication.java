0839b2523de7dc981b33895cfba1f064424f244f
Dongyu Zhang <>
2018-10-27 19:28:45 -0400

    Login Hello World - Spring Boot version

---
+++ LoginSpringBoot/src/main/java/com/dongyuzhang/loginhello/LoginhelloApplication.java
@@ -0,0 +1,12 @@
+package com.dongyuzhang.loginhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginhelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginhelloApplication.class, args);
+    }
+}
---
+++ LoginSpringBoot/src/test/java/com/dongyuzhang/loginhello/LoginhelloApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dongyuzhang.loginhello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginhelloApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
